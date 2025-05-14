5ff425dfccbef4b70f93e393dfdddff01802528a
João Pedro <165161332+Kanjica@users.noreply.github.com>
2025-05-14 09:39:38 -0300

    Update listaComprimida.cpp

--- Estrutura_de_Dados_1/listaComprimida.cpp
+++ Estrutura_de_Dados_1/listaComprimida.cpp
@@ -1,2 +1,51 @@
-#include <iostream.h>
+#include <iostream>
 
+using namespace std;
+
+struct Nodo {
+    int info;
+    Nodo* prox;
+};
+
+class listaComprimida {
+public:
+    listaComprimida() {}
+
+    Nodo* intercalar(Nodo* x, Nodo* y);
+};
+
+Nodo* listaComprimida::intercalar(Nodo* x, Nodo* y) {
+    Nodo* z = nullptr;
+    Nodo* fim = nullptr, 
+    Nodo* ax,* ay;
+
+    if (x == nullptr) {
+        z = y;
+    } else if (y == nullptr) {
+        z = x;
+    } else {
+        while ((x != nullptr) && (y != nullptr)) {
+            ax = x;
+            x = x->prox;
+            ay = y;
+            y = y->prox;
+
+            ax->prox = ay;
+            ay->prox = nullptr;
+
+            if (z == nullptr) {
+                z = ax;
+                fim = ay;
+            } else {
+                fim->prox = ax;
+                fim = ay;
+            }
+        }
+        if (x == nullptr) {
+            fim->prox = y;
+        } else {
+            fim->prox = x;
+        }
+    }
+    return z;
+}
