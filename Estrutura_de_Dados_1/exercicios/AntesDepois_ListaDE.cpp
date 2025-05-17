65f0fc66d101b056ccb41775494dd7df486c6e73
João Pedro <165161332+Kanjica@users.noreply.github.com>
2025-05-16 22:35:35 -0300

    Update AntesDepois_ListaDE.cpp

--- Estrutura_de_Dados_1/exercicios/AntesDepois_ListaDE.cpp
+++ Estrutura_de_Dados_1/exercicios/AntesDepois_ListaDE.cpp
@@ -1,1 +1,39 @@
+#include <iostream>
 
+struct Nodo {
+    int info;
+    Nodo* prox, *ant;
+};
+
+class Lista(){
+  public:
+  Nodo* inicio;
+  Lista(){
+    inicio = nullptr;
+  }
+  void antesN(int n);
+  void depoisN(int n);
+}
+
+void Lista::antesN(int n){
+  if(inicio==nullptr) return;
+  Nodo* novo = new Nodo();
+  
+  novo->info = n;
+  novo->prox == nullptr;
+  novo->ant == nullptr;
+  
+  Nodo* atual = inicio, *ante=nullptr;
+  atual->ant = ante;
+  ante->prox = atual;
+  
+  while(atual!=nullptr && atual->info !=n){
+    atual->ant = ante;
+    ante->prox = atual;
+
+    ante = ante->prox;
+    atual = atual->prox;
+  }
+  
+}
+void Lista::depoisN(int n){}
