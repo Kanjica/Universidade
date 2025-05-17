2fc82b4ad6bfd2c4d997520217b73fbd2235d664
João Pedro <165161332+Kanjica@users.noreply.github.com>
2025-05-17 20:03:17 -0300

    Update palindromo_ListaDE.cpp

--- Estrutura_de_Dados_1/exercicios/palindromo_ListaDE.cpp
+++ Estrutura_de_Dados_1/exercicios/palindromo_ListaDE.cpp
@@ -18,46 +18,35 @@ class Lista{
   }
   void addChar(char c);
   bool isPalindromo();
-}
-void Lista::addChar(char c){
-  Nodo* atual = inicio, *anterior = nullptr;
-  
-  Nodo *novo = new Nodo();
-  novo->info = c;
-  novo->prox = nullptr;
-  novo->ant = nullptr;
-  
-  if(inicio == nullptr){
-    inicio = novo;
-    fim = novo;
-    return;
-  }
-  
-  whle(atual->prox!=nullptr){
-    anterior = atual;
-    atual = atual->prox;
-    atual->ant = anterior;
-    tamanho++;
-  }
-  fim = atual;
- 
-  atual->prox = novo;
-  novo->ant = atual;
-  fim = novo;
-  tamanho+=2;
+};
 
+void Lista::addChar(char c){
+    Nodo* novo = new Nodo();
+    novo->info = c;
+    novo->prox = nullptr;
+    novo->ant = nullptr;
+
+    if(inicio == nullptr){
+        inicio = fim = novo;
+    }
+    else{
+        fim->prox = novo;
+        novo->ant = fim;
+        fim = novo;
+    }
+    tamanho++;
 }
 
 bool Lista::isPalindromo(){
   if(inicio == nullptr){return;}
   
   Nodo* auxInicio = inicio, auxFim = fim;
-  for(int i=0; i<tamanho i++){
-    if(auxInicio->info != auxFim->info){ std::cout<<"Não é palíndromo." std::endl;return;}
+  for(int i=0; i<tamanho/2 i++){
+    if(auxInicio->info != auxFim->info){ std::cout<<"Não é palíndromo." std::endl;return false;}
 
     auxInicio = auxInicio->prox;
     auxFim = auxFim->ant;
   }
   
-  std::cout << "É palíndromo." std::endl; return;
+  std::cout << "É palíndromo." std::endl; return true;
 }
