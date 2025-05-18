648b6535bb98e82b81c8cd785cafe2ca9d9cf52c
João Pedro <165161332+Kanjica@users.noreply.github.com>
2025-05-18 12:04:52 -0300

    Update 01.cpp

--- Estrutura_de_Dados_1/Lista/01.cpp
+++ Estrutura_de_Dados_1/Lista/01.cpp
@@ -7,84 +7,92 @@
 
 //b. retirar todos os elementos pares da lista inicial.
 
-struct Nodo{
-  int info;
-  Nodo *prox;
+#include <iostream>
+
+struct Nodo {
+    int info;
+    Nodo *prox;
 };
 
-class Lista{
-Nodo* inicio;
+class Lista {
+    Nodo* inicio;
 public:
-  Lista(){
-    inicio = nullptr;
-  }
+    Lista() {
+        inicio = nullptr;
+    }
 
-  Nodo *fA(Nodo *inicio);
-  Nodo *fB(Nodo *inicio);
+    Nodo* fA(Nodo *inicio);
+    Nodo* fB(Nodo *inicio);
 };
 
-Nodo* Lista::fA(Nodo*inicio){
-  if (inicio == nullptr){return nullptr;}
-
-  Nodo* atual = inicio;
-  Nodo* novaLista = nullptr;
-  Nodo* auxNovaLista, * ant;
-  
-  int cont;
-  while(atual!=nullptr){
-    //testando se é primo
-    cont = 0;
-    if(atual->info != 0 && atual->info != 1)
-    for(int i = 3; i<atual->info; i+=2){
-      if(atual->info%i == 0){break;cont = 1;}
+bool ehPrimo(int n){
+    if(n <= 1) return false;
+    if(n == 2) return true;
+    if(n % 2 == 0) return false;
+    for(int i = 3; i * i <= n; i += 2){
+        if(n % i == 0) return false;
     }
-
-    //adicionando na lista
-    if(cont==0 && atual->info != 0 && atual->info != 1){
-      if(novaLista == nullptr){
-        novaLista = atual;
-        auxNovaLista = novaLista;
-      }
-      else{
-        auxNovaLista->prox = atual
-        auxNovaLista = auxNovaLista->prox;
-      }
-    }
-    //removendo da principal
-    if(cont==0 && atual->info != 0 && atual->info != 1){
-      if(ant!=nullptr){
-        ant->prox = atual->prox;
-      }
-      else{
-        inicio = atual->prox;
-      }
-    }
-    ant = atual;
-    atual = atual->prox;
-  }
-
-  //ordenanado 
-  auxNovaLista = novaLista;
-  while(auxNovaLista->prox!=nullptr){
-    if(auxNovaLista->info > auxNovaLista->prox->info){
-      Nodo* aux2 = auxNovaLista->prox;
-      auxNovaLista->prox = auxNovaLista;
-      auxNovaLista = aux;
-    }
-    auxNovaLista = auxNovaLista->prox;
-  }
+    return true;
 }
 
-Nodo* Lista::fB(Nodo*inicio){
-  if(inicio == nullptr)return nullptr;
+Nodo* Lista::fA(Nodo *inicio){
+    if(inicio == nullptr) return nullptr;
+
+    Nodo *atual = inicio;
+    Nodo *novaLista = nullptr;
+    Nodo *auxNovaLista = nullptr;
+    Nodo *ant = nullptr;
   
-  Nodo* atual = inicio *ant = nullptr;
-  while(atual != nullptr){
-    if(atual->info%2 == 0){
-      if(ant!=nullptr){ant->prox = atual->prox;}
-      else{inicio = atual ->prox;}
+    while(atual != nullptr){
+        bool ehPrimoImpar = (atual->info % 2 != 0) && ehPrimo(atual->info);
+
+        if (ehPrimoImpar){
+
+            Nodo *novoNo = new Nodo;
+            novoNo->info = atual->info;
+            novoNo->prox = novaLista;
+            novaLista = novoNo;
+
+            if(ant == nullptr){
+                inicio = atual->prox;
+            }
+            else{
+                ant->prox = atual->prox;
+            }
+            Nodo *prox = atual->prox;
+            delete atual; 
+            atual = prox;
+        } 
+        else{
+            ant = atual;
+            atual = atual->prox;
+        }
     }
-    ant = atual;
-    atual = atual->prox;
-  }
+    return novaLista;
+}
+
+Nodo* Lista::fB(Nodo *inicio){
+    if(inicio == nullptr) return nullptr;
+  
+    Nodo *atual = inicio;
+    Nodo *ant = nullptr;
+  
+    while(atual != nullptr){
+        if(atual->info % 2 == 0){
+            if(ant == nullptr){
+                inicio = atual->prox;
+            } 
+            else{
+                ant->prox = atual->prox;
+            }
+            Nodo *prox = atual->prox;
+            delete atual; 
+            atual = prox;
+        }
+        else{
+            ant = atual;
+            atual = atual->prox;
+        }
+    }
+    return inicio;
 }
