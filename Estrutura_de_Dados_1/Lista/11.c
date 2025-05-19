849024fdd07139eabc261f64721150bde2c96938
João Pedro <165161332+Kanjica@users.noreply.github.com>
2025-05-19 08:56:30 -0300

    Update 11.c

--- Estrutura_de_Dados_1/Lista/11.c
+++ Estrutura_de_Dados_1/Lista/11.c
@@ -1,28 +1,37 @@
 typedef struct Nodo{
-  int data;
-  struct Nodo *next, *previous;
+    int data;
+    struct Nodo *next, *previous;
 }Nodo;
 
-Nodo * listK(Nodo *list){
-  if(list==NULL) return NULL;
+Nodo* listK(Nodo *list){
+    if(list == NULL) return NULL;
 
-  Nodo *newList = NULL;
-  Nodo *tail = NULL;
-  Nodo *current = list;
+    Nodo *newList = NULL;
+    Nodo *tail = NULL;
+    Nodo *current = list;
 
-  while(current->next != NULL){
-    current = current->next;
-  }
-  tail = current;
-  current = list;
-  newList = current;
-  
-  while(current != tail){
-    current->previous = tail;
-    tail->previous = current->next;
-    
-    tail = tail->previous;
-    current = current->next;
-  }
-  return newList;
+    while(current->next != NULL){
+        current = current->next;
+    }
+    tail = current;
+
+    current = list;
+    newList = current;
+
+    while(current != tail && current->next != tail){
+        current->previous = tail;
+        tail->previous = current->next;
+
+        current = current->next;
+        tail = tail->previous;
+    }
+
+    if(current == tail){
+        current->previous = NULL;
+    } 
+    else if(current->next == tail){
+        current->previous = tail;
+        tail->previous = NULL;
+    }
+    return newList;
 }
