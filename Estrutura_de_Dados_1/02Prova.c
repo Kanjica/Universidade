56abaa5f586844be237d25b75fd9e9bbd193c38d
João Pedro <165161332+Kanjica@users.noreply.github.com>
2025-05-21 10:17:39 -0300

    Update 02Prova.c

--- Estrutura_de_Dados_1/02Prova.c
+++ Estrutura_de_Dados_1/02Prova.c
@@ -3,60 +3,95 @@
 using namespace std;
 
 void limparTela(){ 
-    std::cout << "\033[2J\033[1;1H";
+    cout << "\033[2J\033[1;1H";
 }
 
-struct Nodo {
-	char *info;
-	struct Nodo *prox, *ant;
+struct Nodo{
+    int info; 
+    Nodo *prox, *ant;
 };
 
-class Lista(){
-  public:
-  Nodo* inicio;
-  Lista(){
-    inicio = nullptr;
-    inicio->prox = nullptr;
-    inicio->ant = nullptr;
-  }
+class Lista{
+public:
+    Nodo* inicio;
+    Lista(){
+        inicio = nullptr;
+    }
   
-  void add(int n);
-  void printarElementos();
-  void removerAntesDepois(int n);
-}
+    void add(int n);
+    void remover(int n); 
+    void printarElementos();
+    void removerAntesDepois(int n);
+};
 
 void Lista::add(int n){
-   if(inicio == nullptr){
-     inicio->info = n;
-   }
-   else{
-     Nodo* atual = inicio;
-     while(atual != nullptr){}
-   }
-}
-
-void Lista::printarElementos(){
-  if(inicio == nullptr) return;
-
-  Nodo* atual = inicio;
-  
-  while(atual!=nullptr){
+    Nodo* novo = new Nodo();
+    novo->info = n;
+    novo->prox = nullptr;
+    novo->ant = nullptr;
     
-  }
+    if(inicio == nullptr){
+        inicio = novo;
+    } 
+    else{
+        Nodo* atual = inicio;
+        while(atual->prox != nullptr){
+            atual = atual->prox;
+        }
+        atual->prox = novo;
+        novo->ant = atual;
+    }
 }
+
+void Lista::remover(int n){
+    if(inicio == nullptr) return;
+    
+    
+}
+void Lista::printarElementos(){
+    if(inicio == nullptr){cout << "Lista vazia!" << endl;return;}
+
+    Nodo* atual = inicio;
+    cout << "Lista: ";
+    while(atual != nullptr) {
+        cout << atual->info << " ";
+        atual = atual->prox;
+    }
+    cout << endl;
+}
+
 
 void Lista::removerAntesDepois(int n){
-  if(inicio == nullptr) return;
+    if(inicio == nullptr) return;
 
-  Nodo* atual = inicio;
-  Nodo* antecessor = nullptr;
-
-  while(atual!= nullptr)
+    Nodo* atual = inicio;
+    
+    while(atual != nullptr){
+        if(atual->info == n){
+            if(atual->ant != nullptr){
+                Nodo* aux = atual->ant;
+                if(aux == inicio){ inicio = atual; }
+		    
+                atual->ant = aux->ant;
+                if(aux->ant != nullptr){ aux->ant->prox = atual; }
+		    
+                delete aux;
+            }
+            
+            if(atual->prox != nullptr){
+                Nodo* aux = atual->prox;
+                atual->prox = aux->prox;
+                if(aux->prox != nullptr){ aux->prox->ant = atual; }
+                delete aux;
+            }
+        }
+        atual = atual->prox;
+    }
 }
 
-int main(void){
-  Lista lista = new Lista();
+int main(){
+    Lista lista = new Lista();
 
-  //menu
-  return 0;
+
+    return 0;
 }
