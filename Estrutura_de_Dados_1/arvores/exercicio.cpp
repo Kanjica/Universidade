a81e0a84208726addbe9cc658eac22753d09950d
João Pedro <165161332+Kanjica@users.noreply.github.com>
2025-06-27 17:31:22 -0300

    Update exercicio.cpp

--- Estrutura_de_Dados_1/arvores/exercicio.cpp
+++ Estrutura_de_Dados_1/arvores/exercicio.cpp
@@ -15,40 +15,68 @@ class Arvore {
 public:
     Node* raiz;
 
-    Arvore() {
-        raiz = nullptr;
+    Arvore(): raiz(nullptr) {}
+
+    void inserir(int valor){
+        raiz = inserirRec(raiz, valor);
     }
 
-    void setRaiz(Node* no) {
-        raiz = no;
+    void exibirEmOrdem(){
+        exibirEmOrdemRec(raiz);
     }
 
-    Node* inserir(Node* raiz, int valor) {
-    if(!raiz){
-        return new Node(valor);
+    bool buscar(int valor){
+        return buscarRec(raiz, valor) != nullptr;
     }
 
-    if(valor < raiz->data){
-        raiz->left = inserir(raiz->left, valor); 
-    } 
-    else if(valor > raiz->data){
-        raiz->right = inserir(raiz->right, valor); 
+private:
+    Node* inserirRec(Node* raiz, int valor) {
+        if(!raiz){
+            return new Node(valor);
+        }
+    
+        if(valor < raiz->data){
+            raiz->left = inserirRec(raiz->left, valor); 
+        } 
+        else if(valor > raiz->data){
+            raiz->right = inserirRec(raiz->right, valor); 
+        }
+        else{
+            cout << "Valor duplicado ignorado: " << valor << endl;
+        }
+        return raiz; 
     }
-    else{
-        cout << "Valor duplicado ignorado: " << valor << endl;
-    }
-    return raiz; 
-}
 
+    void exibirEmOrdemRec(Node* raiz){
+        if(!raiz) return;
+
+        exibirEmOrdemRec(raiz->left);
+        cout << raiz->data << " " <<endl;
+        exibirEmOrdemRec(raiz->right);
+    }
+
+    Node* buscarRec(Node* raiz, int valor){
+        if(!raiz || raiz->data == valor) return raiz;
+        if(valor < raiz->data) return buscarRec(raiz->left, valor);
+        else return buscarRec(raiz->right, valor);
+    }
 };
 
 int main() {
     Arvore arv;
-    Node* novoNo = new Node(10);
-    arv.setRaiz(novoNo);
     
-    arv.inserir(arv.raiz, 5);
-    
-    cout << "Raiz: " << arv.raiz->data << endl;
+    arv.inserir(10);
+    arv.inserir(5);
+    arv.inserir(15); 
+    arv.inserir(3); 
+    arv.inserir(7); 
+    arv.inserir(12); 
+    arv.inserir(18); 
+
+    arv.exibirEmOrdem();
+
+    cout << "Buscar 7: " << (arv.buscar(7) ? "Encontrado" : "Não encontrado") << endl;
+    cout << "Buscar 99: " << (arv.buscar(99) ? "Encontrado" : "Não encontrado") << endl;
+
     return 0;
 }
