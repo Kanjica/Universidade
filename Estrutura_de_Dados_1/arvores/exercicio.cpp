6826753279a03fe07864f77e5581d0c1774c0365
João Pedro <165161332+Kanjica@users.noreply.github.com>
2025-06-27 14:22:13 -0300

    Update exercicio.cpp

--- Estrutura_de_Dados_1/arvores/exercicio.cpp
+++ Estrutura_de_Dados_1/arvores/exercicio.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
 
-struct Node{
-  int data;
-  Node* left, *right;
-}Node;
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int valor) : data(valor), left(nullptr), right(nullptr) {}
+    Node() : data(0), left(nullptr), right(nullptr) {}
+};
+
+class Arvore {
+public:
+    Node* raiz;
+
+    Arvore() {
+        raiz = nullptr;
+    }
+
+    void setRaiz(Node* no) {
+        raiz = no;
+    }
+};
+
+int main() {
+    Arvore arv;
+    Node* novoNo = new Node(10);
+    arv.setRaiz(novoNo);
+
+    std::cout << "Raiz: " << arv.raiz->data << std::endl;
+    return 0;
+}
